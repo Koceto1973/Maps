3ca41f63cc417589aa0bf7668bfccd7c234f06cd
K.K <koceto1973@yahoo.ca>
2018-09-26 17:53:27 +0300

    initial commit

---
+++ Maps/Controller/HomeVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Maps
+//
+//  Created by К.К. on 26.09.18.
+//  Copyright © 2018 TeamK. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
