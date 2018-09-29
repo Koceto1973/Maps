b7ae6a2717cce4691bf3e9ea10d98719afa291ec
K.K <koceto1973@yahoo.ca>
2018-09-29 16:48:10 +0300

    NavigatorVC completed

--- Maps/Controller/NavigatorVC.swift
+++ Maps/Controller/NavigatorVC.swift
@@ -13,11 +13,37 @@ import MapKit
 class NavigatorVC: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
     @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var latitude: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var longitude: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var speed: UILabel!
+    @IBOutlet weak var courseLabel: UILabel!
+    @IBOutlet weak var course: UILabel!
+    @IBOutlet weak var altitudeLabel: UILabel!
+    @IBOutlet weak var altitude: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var address: UILabel!
+    
+   
+    @IBOutlet weak var detailsButton: uiButton!
+    @IBAction func detailsClick(_ sender: Any) {
+        if detailsFlag {
+            detailsButton.setTitle("Show Details", for: UIControl.State.normal)
+        } else {
+            detailsButton.setTitle("Hide Details", for: UIControl.State.normal)
+        }
+        
+        ToggleDetails(with: detailsFlag)
+    }
+    
     
     var lds:locationDataSet = locationDataSet.init(lat: 0, lon: 0, latD: 1, lonD: 1)
     
     var locationManager = CLLocationManager()
     
+    var detailsFlag:Bool = true
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,6 +53,7 @@ class NavigatorVC: UIViewController, CLLocationManagerDelegate, MKMapViewDelegat
         locationManager.requestAlwaysAuthorization()
         locationManager.startUpdatingLocation()
         
+        ToggleDetails(with: detailsFlag)
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -35,11 +62,11 @@ class NavigatorVC: UIViewController, CLLocationManagerDelegate, MKMapViewDelegat
         self.map.setRegion(lds.region, animated: true)
         
         let location = locations[0]
-        //self.latitudeLabel.text = String(location.coordinate.latitude)
-        //self.longitudeLabel.text = String(location.coordinate.longitude)
-        //self.courseLabel.text = String(location.course)
-        //self.speedLabel.text = String(location.speed)
-        //self.altitudeLabel.text = String(location.altitude)
+        self.latitudeLabel.text = String(location.coordinate.latitude)
+        self.longitudeLabel.text = String(location.coordinate.longitude)
+        self.courseLabel.text = String(location.course)
+        self.speedLabel.text = String(location.speed)
+        self.altitudeLabel.text = String(location.altitude)
         
         CLGeocoder().reverseGeocodeLocation(location) { (placemarks, error) in
             if error != nil { print(error!) } else {
@@ -52,9 +79,28 @@ class NavigatorVC: UIViewController, CLLocationManagerDelegate, MKMapViewDelegat
                     if placemark.postalCode != nil            { address += placemark.postalCode! + "\n"            }
                     if placemark.country != nil               { address += placemark.country! + "\n"               }
                     
-                    print(address)  //self.addressLabel.text = address
+                    self.addressLabel.text = address
                }
             }
         }
+    }
+    
+    func ToggleDetails(with detailsFlag:Bool){
+        self.detailsFlag = !detailsFlag
+        
+        latitudeLabel.isHidden = detailsFlag
+        latitude.isHidden = detailsFlag
+        longitudeLabel.isHidden = detailsFlag
+        longitude.isHidden = detailsFlag
+        speedLabel.isHidden = detailsFlag
+        speed.isHidden = detailsFlag
+        courseLabel.isHidden = detailsFlag
+        course.isHidden = detailsFlag
+        altitudeLabel.isHidden = detailsFlag
+        altitude.isHidden = detailsFlag
+        addressLabel.isHidden = detailsFlag
+        address.isHidden = detailsFlag
+        
+        
     }
 }
