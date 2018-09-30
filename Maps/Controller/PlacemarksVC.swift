2b02067f26048aeebcff9519cb1797de6f16ad69
K.K <koceto1973@yahoo.ca>
2018-09-30 21:34:18 +0300

    PlacemarksVC added with it's class file

---
+++ Maps/Controller/PlacemarksVC.swift
@@ -0,0 +1,30 @@
+//
+//  PlacemarksVC.swift
+//  Maps
+//
+//  Created by К.К. on 30.09.18.
+//  Copyright © 2018 TeamK. All rights reserved.
+//
+
+import UIKit
+
+class PlacemarksVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
