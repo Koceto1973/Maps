da27101dde3dac2c627e748c47d838bc70d0f13c
K.K <koceto1973@yahoo.ca>
2018-09-30 23:15:32 +0300

    Controller/PlacemarksVC: tableView added and configured to display placemarks; Back button aligned with the tableView

--- Maps/Controller/PlacemarksVC.swift
+++ Maps/Controller/PlacemarksVC.swift
@@ -8,18 +8,33 @@
 
 import UIKit
 
-class PlacemarksVC: UIViewController {
+class PlacemarksVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
     
     var placemarks = Placemarks.init()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         // reading the permanent storage
         if let data = UserDefaults.standard.object(forKey: "placemarks") as? Placemarks {
             self.placemarks = data
         }
         
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { return placemarks.number }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "cell" )
+        cell.textLabel?.text = String(placemarks.Get(item: 1, of: indexPath.row))
+        
+        return cell
     }
     
     // UserDefaults.standard.set("data", forKey: "keyToData")
